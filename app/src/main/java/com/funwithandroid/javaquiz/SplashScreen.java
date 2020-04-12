af655c1e07af64ca010b56155df1c8e9db9730ff
Prem Prakash <42450106+prem2033@users.noreply.github.com>
2020-04-12 16:47:35 +0530

    Splash Screen added

---
+++ app/src/main/java/com/funwithandroid/javaquiz/SplashScreen.java
@@ -0,0 +1,20 @@
+package com.funwithandroid.javaquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+       // setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_splash_screen);
+        Intent intent=new Intent(this,recyler_view_quiz_main_page.class);
+        startActivity(intent);
+        finish();
+    }
+}
